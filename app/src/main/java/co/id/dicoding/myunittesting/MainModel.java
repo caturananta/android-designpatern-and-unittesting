db7467f6e175811e358c12f22b5c7d7a4cb22db3
catur <caturanantaa@gmail.com>
2019-08-25 20:34:12 +0700

    first

---
+++ app/src/main/java/co/id/dicoding/myunittesting/MainModel.java
@@ -0,0 +1,11 @@
+package co.id.dicoding.myunittesting;
+
+public class MainModel {
+    private double volume;
+    public MainModel(double volume) {
+        this.volume = volume;
+    }
+    public double getVolume() {
+        return volume;
+    }
+}
---
+++ app/src/main/java/co/id/dicoding/myunittesting/MainView.java
@@ -0,0 +1,5 @@
+package co.id.dicoding.myunittesting;
+
+public interface MainView {
+    void showVolume(MainModel model);
+}
